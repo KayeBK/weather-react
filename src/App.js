1274e24cee4d52fe335fa4b9d5fe634af146f936
kaye kinser <kaly@kinser.us>
2023-03-11 13:22:24 -0600

    Moving code from App.js to Weather.js

--- src/App.js
+++ src/App.js
@@ -8,43 +8,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="row header">
-          <Time time="12PM" calendar="Feb 17 2023" />
-
-          <div className="col-5">
-            <div className="d-flex search-form">
-              <span className="units">
-                <a href="/#" id="celsius" className="active">
-                  °C
-                </a>
-                |
-                <a href="/#" id="fahrenheit">
-                  °F
-                </a>
-              </span>
-              <form className="city-search" id="city-search">
-                <input
-                  type="search"
-                  placeholder="Enter a city"
-                  className="enter-city"
-                  autoComplete="off"
-                />
-                <input
-                  type="submit"
-                  value="Search"
-                  className="search-button formControl"
-                />
-                <input
-                  type="button"
-                  value="📍"
-                  className="current-location-emoji"
-                />
-              </form>
-            </div>
-          </div>
-        </div>
-
-        <City name="San Antonio" condition="Clear sky" temperature={19} />
         <Weather high={34} low={19} humidity={10} />
         <WeatherForecast />
       </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,45 @@
 import React from "react";
+import Time from "./Time";
+import City from "./City";
 
 export default function Weather(props) {
   return (
+    <div className="row header">
+    <Time time="12PM" calendar="Feb 17 2023" />
+
+    <div className="col-5">
+      <div className="d-flex search-form">
+        <span className="units">
+          <a href="/#" id="celsius" className="active">
+            °C
+          </a>
+          |
+          <a href="/#" id="fahrenheit">
+            °F
+          </a>
+        </span>
+        <form className="city-search" id="city-search">
+          <input
+            type="search"
+            placeholder="Enter a city"
+            className="enter-city"
+            autoComplete="off"
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="search-button formControl"
+          />
+          <input
+            type="button"
+            value="📍"
+            className="current-location-emoji"
+          />
+        </form>
+      </div>
+    </div>
+
+  <City name="San Antonio" condition="Clear sky" temperature={19} />
     <div>
       <div className="condition-list">
         <ul>
@@ -10,6 +48,7 @@ export default function Weather(props) {
           <li>Humidity: {props.humidity}%</li>
         </ul>
       </div>
+    </div>
     </div>
   );
 }
