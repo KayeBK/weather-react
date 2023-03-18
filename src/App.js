5f3f871b58f6c0ae1715cb8f778e1bef7b77f8e8
kaye kinser <kaly@kinser.us>
2023-03-17 23:15:56 -0500

    Removed the unit conversion from main temp

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       </div>
 
       <div className="App-link-footer">
-        <a href="https://github.com/KayeBK/weather-react" target="_blank" rel="noreferrer">Open-source code on GitHub</a>, developed by Kaye Kinser.
+        Developed by Kaye Kinser, open-sourced on <a href="https://github.com/KayeBK/weather-react" target="_blank" rel="noreferrer">GitHub</a> and hosted on <a href="https://teal-cheesecake-72006b.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
       </div>
     </div>
   );
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,29 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-    const [unit, setUnit] = useState("celsius");
-    function showFahrenheit(event) {
-        event.preventDefault();
-        setUnit("fahrenheit");
-    }
-
-    function showCelsius(event) {
-        event.preventDefault();
-        setUnit("celsius");
-    }
-
-    if (unit === `celsius`) {
-        return (
-            <div className="WeatherTemperature">
-            <span className="Weather-main-temperature">{Math.round(props.celsius)}</span><span className="Weather-units">°C | <a href="/" onClick={showFahrenheit}>°F</a></span>
-            </div>
-    );
-} else {
-    let fahrenheit = (props.celsius * 9/5) + 32;
     return (
         <div className="WeatherTemperature">
-            <span className="Weather-main-temperature">{Math.round(fahrenheit)}</span><span className="Weather-units"><a href="/" onClick={showCelsius}>°C</a> | °F</span>
-            </div>
-    );
-}
+        <span className="Weather-main-temperature">{Math.round(props.celsius)}</span><span className="Weather-units">°C</span>
+        </div>
+);
 }
